3c712fd3139f34025e25f341aae804a429b0ec17
Nazareno Andrade <nazareno@computacao.ufcg.edu.br>
2016-09-24 17:34:04 -0300

    Gráficos de barra comparando tipos de receita

---
+++ doacoes.R
@@ -0,0 +1,53 @@
+library(dplyr)
+library(ggplot2)
+library(rcdimple)
+
+le_receitas = function(arquivo){
+    require(readr)
+    readr::read_csv2(arquivo, 
+                                readr::locale(encoding = "Latin1"), 
+                                col_names = TRUE, 
+                                col_types = NULL) %>% 
+    return()
+}
+
+capwords <- function(s, strict = FALSE) {
+    cap <- function(s) paste(toupper(substring(s, 1, 1)),
+                             {s <- substring(s, 2); if(strict) tolower(s) else s},
+                             sep = "", collapse = " " )
+    sapply(strsplit(s, split = " "), cap, USE.NAMES = !is.null(names(s)))
+}
+
+plota_cidade_ggplot = function(as_receitas, a_cidade, o_cargo = "Prefeito"){
+    as_receitas %>% 
+        filter(`Nome da UE` == a_cidade, Cargo == o_cargo) %>% 
+        group_by(`Nome candidato`, `Tipo receita`) %>% 
+        summarise(Recebido = sum(`Valor receita`)) %>% 
+        ggplot(aes(x = `Nome candidato`, y = Recebido, fill = `Tipo receita`, group = `Tipo receita`)) + 
+        geom_bar(stat = "identity", 
+                 position = "dodge") + 
+        coord_flip() %>% 
+        return()
+}
+
+plota_cidade_dimple = function(as_receitas, a_cidade, o_cargo = "Prefeito"){
+    r = as_receitas %>% 
+        filter(`Nome da UE` == a_cidade, Cargo == o_cargo) %>% 
+        group_by(`Nome candidato`, `Tipo receita`) %>% 
+        summarise(Recebido = sum(`Valor receita`)) %>%
+        rename(Candidato = `Nome candidato`, Tipo = `Tipo receita`)
+    dimple(
+        Candidato ~ Recebido,
+        groups = "Tipo",
+        data = r,
+        type = "bar", 
+        bounds = list(x=250,y=30,width=350,height=330)
+    ) %>%
+        xAxis(type = "addMeasureAxis") %>%
+        yAxis(type = "addCategoryAxis") %>%
+        add_legend(
+            x = "60%", width = "30%", y = "10%", height = 100 
+        ) %>% 
+        return()
+}
+
